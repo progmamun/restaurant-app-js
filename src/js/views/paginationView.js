403c2540864c198b2ba95fe700a60b11f2a7eeb7
progmamun <mamun333089@gmail.com>
2022-03-05 16:37:24 +0600

    pagination

---
+++ src/js/views/paginationView.js
@@ -0,0 +1,36 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg';
+
+class ResultsView extends View {
+  _parentElement = document.querySelector('.pagination');
+
+  _generateMarkup() {
+    const curPage = this._data.page;
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+    console.log(numPages);
+    // page 1. and there are other pages
+    if (curPage === 1 && numPages > 1) {
+      return 'Page 1, others';
+    }
+    // Last page
+    if (curPage === numPages && numPages > 1) {
+      return `<button class="btn--inline pagination__btn--prev">
+      <svg class="search__icon">
+        <use href="src/img/icons.svg#icon-arrow-left"></use>
+      </svg>
+      <span>Page ${curPage - 1}</span>
+    </button>
+    `;
+    }
+    // Other page
+    if (curPage < numPages) {
+      return 'other page';
+    }
+    // page 2, and there are No other pages
+    return 'only 1 page';
+  }
+}
+
+export default new PaginationView();
