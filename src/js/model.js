ea536d1310e77f7d64179e9bf6c85344cdca1a7e
progmamun <mamun333089@gmail.com>
2022-03-13 21:26:55 +0600

    updating-recipe-servings

--- src/js/model.js
+++ src/js/model.js
@@ -64,3 +64,12 @@ export const getSearchResultsPage = function (page = state.search.page) {
 
   return state.search.results.slice(start, end);
 };
+
+export const updateServings = function (newServings) {
+  state.recipe.ingredients.foreach(ing => {
+    ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
+    // newQt = oldQt * newServings / oldServings // 2 * 8 / 4 = 4
+  });
+
+  state.recipe.servings = newServings;
+};
