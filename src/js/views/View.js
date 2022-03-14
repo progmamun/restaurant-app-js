f20b36e7cf11c60b055bb691ef2603761fb2aaf1
progmamun <mamun333089@gmail.com>
2022-03-14 11:16:08 +0600

    dom-algorithm-developing

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -4,15 +4,31 @@ import icons from 'url:../../img/icons.svg'; // parcel version 2
 export default class View {
   _data;
 
-  render(data) {
+  update(data) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
 
     this._data = data;
-    const markup = this_generateMarkup();
-    this._clear();
-    this._parentElement.insertAdjacentHTML('afterbegin', markup);
-  }
+    const newMarkup = this_generateMarkup();
+
+    const newDOM = document.createRange().createContextualFragment(newMarkup);
+    const newElements = Array.from(newDOM.querySelectorAll('*'));
+    const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+
+      if(
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== '';
+      ) {
+        curEl.textContent = newEl.textContent;
+      }
+    });
+}
+    // this._clear();
+    // this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  // }
 
   _clear() {
     this._parentElement.innerHTML = '';
