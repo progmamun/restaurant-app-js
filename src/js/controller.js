eb33b035dbe7ec6ee5b5ad551c4fc26f9b6ed9b5
progmamun <mamun333089@gmail.com>
2022-03-14 09:46:33 +0600

    updating-recipe-servings

--- src/js/controller.js
+++ src/js/controller.js
@@ -60,9 +60,9 @@ const controlPagination = function (goToPage) {
   paginationView.render(model.state.search);
 };
 
-const controlServings = function () {
+const controlServings = function (newServings) {
   // update the recipe servings (in state)
-  model.updateServings(6);
+  model.updateServings(newServings);
 
   // update the recipe view
   recipeView.render(model.state.recipe);
@@ -70,6 +70,7 @@ const controlServings = function () {
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
