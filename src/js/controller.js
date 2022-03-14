a66e6942c214db2375f23eb146bc4123c2b6fa9f
progmamun <mamun333089@gmail.com>
2022-03-14 16:23:09 +0600

    bookmarks

--- src/js/controller.js
+++ src/js/controller.js
@@ -73,6 +73,8 @@ const controlServings = function (newServings) {
 };
 
 const controlAddBookmark = function () {
+  if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
+  if (model.state.recipe.bookmarked) model.addBookmark(model.state.id);
   model.addBookmark(model.state.recipe);
   recipeView.update(model.state.recipe);
 };
--- src/js/model.js
+++ src/js/model.js
@@ -27,6 +27,10 @@ export const loadRecipe = async function (id) {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
+
+    if (state.bookmarks.some(bookmark => bookmark.id === id))
+      state.recipe.bookmarked = true;
+    else state.recipe.bookmarked = false;
   } catch (err) {
     // Temp error handling
     console.error(`${err}💥`);
@@ -73,4 +77,20 @@ export const updateServings = function (newServings) {
   });
 
   state.recipe.servings = newServings;
+};
+export const addBookmark = function (recipe) {
+  // Add bookmark
+  state.bookmarks.push(recipe);
+
+  // Mark current recipe as bookmarked
+  if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+};
+
+export const deleteBookmark = function (id) {
+  // Delete bookmark
+  const index = state.bookmarks.findIndex(el => el.id === id);
+  state.bookmarks.splice(index, 1);
+
+  // Mark current recipe as NOT bookmarked
+  if (id === state.recipe.id) state.recipe.bookmarked = false;
 };
